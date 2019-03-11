f8cad99864c18a2d23b9489ac5d9889728e82af8
Estella Song <estellajaysong@gmail.com>
2019-03-11 23:51:35 +0000

    completed step 6, created secrets file and required it

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,19 @@
 var request = require('request');
+var secrets = require('./secrets');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) 
 {
+  var options = 
+  {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: 
+    {
+      'User-Agent': 'request',
+      'Authorization': secrets.GITHUB_TOKEN
+    }
+  };
   var repoOwner = process.argv[2];
   var repoName = process.argv[3];
 }
@@ -12,3 +22,5 @@ function getRepoContributors(repoOwner, repoName, cb)
     console.log("Errors:", err);
     console.log("Result:", result);
   });
+
+
