971ef55aee289e89c7c5e8dfefa72ce731e952e2
Estella Song <estellajaysong@gmail.com>
2019-03-11 23:36:29 +0000

    completed step 3, started getRepoContributors function

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,14 @@
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) 
+{
+  var repoOwner = process.argv[2];
+  var repoName = process.argv[3];
+}
+
+  getRepoContributors("jquery", "jquery", function(err, result) {
+    console.log("Errors:", err);
+    console.log("Result:", result);
+  });
